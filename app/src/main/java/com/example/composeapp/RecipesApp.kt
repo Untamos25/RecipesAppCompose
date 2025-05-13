250e623394acfd626f92148555825ebab8cb266f
Stanislav <pavlushinsa18@gmail.com>
2025-05-13 17:08:40 +0300

    RCA-08: Настроена навигация между экранами

--- app/src/main/java/com/example/composeapp/RecipesApp.kt
+++ app/src/main/java/com/example/composeapp/RecipesApp.kt
@@ -1,131 +1,97 @@
 package com.example.composeapp
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import com.example.composeapp.model.ScreenId
+import com.example.composeapp.ui.navigation.BottomNavigation
 import com.example.composeapp.ui.theme.RecipesAppTheme
 import com.example.composeapp.ui.theme.recipesAppTypography
 
 @Composable
 fun RecipesApp() {
     RecipesAppTheme {
-        Greeting()
-    }
-}
 
+        var currentScreen by remember { mutableStateOf(ScreenId.CATEGORIES) }
 
-@Composable
-fun Greeting() {
-    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding)
-        ) {
-            Text(
-                text = "Заголовок (displayLarge)",
-                style = recipesAppTypography.displayLarge
-            )
-            Text(
-                text = "Заголовок средний (titleMedium)",
-                style = recipesAppTypography.titleMedium
-            )
-            Text(
-                text = "Тело средний (bodyMedium)",
-                style = recipesAppTypography.bodyMedium
-            )
-            Text(
-                text = "Тело маленький (bodySmall)",
-                style = recipesAppTypography.bodySmall
-            )
-            Text(
-                text = "лейбл (labelLarge)",
-                style = recipesAppTypography.labelLarge
-            )
-
-            Button(
-                onClick = { }
-            ) {
-                Text("КАТЕГОРИИ")
-            }
-
-            HorizontalDivider(thickness = 5.dp)
-
-            Button(
-                onClick = {},
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = MaterialTheme.colorScheme.secondary,
+        Scaffold(
+            bottomBar = {
+                BottomNavigation(
+                    onCategoriesClick = { currentScreen = ScreenId.CATEGORIES },
+                    onFavoriteClick = { currentScreen = ScreenId.FAVORITES }
                 )
-            ) {
-                Text("ИЗБРАННОЕ")
             }
-
-            ColorDemonstration(
-                text = "Цвет ползунка прогресса порций",
-                backgroundColor = MaterialTheme.colorScheme.tertiary,
-            )
-
-            ColorDemonstration(
-                text = "Цвет слайдера порций",
-                backgroundColor = MaterialTheme.colorScheme.tertiaryContainer,
-            )
-
-            ColorDemonstration(
-                text = "Фон карточек и разделителей",
-                backgroundColor = MaterialTheme.colorScheme.surface,
-            )
-
-            ColorDemonstration(
-                text = "Разделители и неактивные элементы",
-                backgroundColor = MaterialTheme.colorScheme.surfaceVariant,
-            )
+        ) { innerPadding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+            ) {
+                when (currentScreen) {
+                    ScreenId.CATEGORIES -> CategoriesScreen()
+                    ScreenId.FAVORITES -> FavoritesScreen()
+                }
+            }
         }
     }
 }
 
 @Composable
-fun ColorDemonstration(text: String, backgroundColor: androidx.compose.ui.graphics.Color) {
-    Column(modifier = Modifier.fillMaxWidth()) {
-        Spacer(Modifier.height(12.dp))
+fun CategoriesScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
         Text(
-            text = text,
-            style = MaterialTheme.typography.labelSmall
+            text = "Экран Категорий",
+            style = recipesAppTypography.displayLarge
         )
-        Surface(
-            color = backgroundColor,
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(50.dp)
-        ) { }
     }
 }
 
-@Preview(name = "LightTheme", showBackground = true)
+@Composable
+fun FavoritesScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Экран Избранного",
+            style = recipesAppTypography.displayLarge
+        )
+    }
+}
+
+@Preview(
+    name = "LightTheme",
+    showBackground = true
+)
 @Composable
 fun RecipesAppLightPreview() {
-    RecipesAppTheme(darkTheme = false) {
+    RecipesAppTheme {
         RecipesApp()
     }
 }
 
-@Preview(name = "DarkTheme", showBackground = true)
+@Preview(
+    name = "DarkTheme",
+    showBackground = true,
+    uiMode = android.content.res.Configuration.UI_MODE_NIGHT_YES
+)
 @Composable
 fun RecipesAppDarkPreview() {
-    RecipesAppTheme(darkTheme = true) {
+    RecipesAppTheme {
         RecipesApp()
     }
 }
